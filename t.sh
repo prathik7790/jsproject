f7ba8419e18f05955abb8b4abb27ef3fd4c762d1
prathik <prathik7790@gmail.com>
2017-09-29 13:05:38 -0500

    t.sh modified

--- t.sh
+++ t.sh
@@ -5,9 +5,9 @@ echo "age"
 read age
 if [ "$age" -le 20 ]
  then
-   echo "$name you are young"
+   echo "$name you are younger"
  else [ "$age" -ge 20 ]
-   echo "$name you are old"
+   echo "$name you are older"
 fi
 
 
